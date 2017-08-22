004b61d52c6d72e7d446a4c23955116fbc402815
Kate.Timoshkina <kate.timoshkina@logicify.com>
2017-08-22 12:50:52 +0300

    Api Service implemented

---
+++ src/app/shared/services/api.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+const API_SERVER = 'https://bookshelf-test-project-7fed0.firebaseio.com/';
+
+@Injectable()
+export class ApiService {
+
+  constructor(private http: Http) { }
+
+  private getApiUrl(endPoint: string, authToken?: string) {
+    const queryTail = !!authToken ? '?auth=' + authToken : '';
+    return API_SERVER + endPoint + '.json' + queryTail;
+  }
+
+  public get(endPoint: string, authToken?: string): Observable<Response> {
+    const url = this.getApiUrl(endPoint, authToken);
+    return this.http.get(url);
+  }
+
+  public put(endPoint: string, data: any, authToken?: string): Observable<Response> {
+    const url = this.getApiUrl(endPoint, authToken);
+    return this.http.put(url, data);
+  }
+
+  public post(endPoint: string, data: any, authToken?: string): Observable<Response> {
+    const url = this.getApiUrl(endPoint, authToken);
+    return this.http.post(url, data);
+  }
+
+}
